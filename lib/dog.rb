3a070bb23f015253263a2a62b7c133a71ee7882f
Richard Loera <richexp9@gmx.com>
2018-01-02 11:03:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,8 @@
-class Dog
-  def bark
-    puts "Woof!"
-  end
-  def sit
-    puts "The dog is sitting."
-end
-
-class Person
-  def talk
-    puts "Hello World!"
-  end
-  def walk
-    puts "The person is walking."
-  end
-end
+# class Dog
+#   def bark
+#     puts "Woof!"
+#   end
+#   def sit
+#     puts "The dog is sitting."
+# end
+#
