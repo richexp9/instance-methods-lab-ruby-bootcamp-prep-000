a34f28eafd86eb55cfdab6730f66ba1321189d59
Richard Loera <richexp9@gmx.com>
2018-01-02 11:00:26 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+  def sit
+    puts "The dog is sitting."
+end
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The person is walking."
+  end
+end
